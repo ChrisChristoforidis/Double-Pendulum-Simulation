f174144dbfcfe557b9008dbba7de297b969d4881
Christos Christoforidis <christoforidisa@yahoo.gr>
2019-09-04 17:16:46 +0200

    extra ui control added

--- Matlab/dp_analysis.m
+++ Matlab/dp_analysis.m
@@ -1,24 +1,25 @@
 clear all
 clc
 a=0.5;
-b=1;
+b=0.5;
 
 m1=20;
 m2=80;
 L1=1;
-L2=0.25;
+L2=0.5;
 grav=-1;
 
-[dX,v,acc]=dp_TMT_EOM(m1,m1,a,b,L1,L2,grav);
+[dX,v,acc]=dp_TMT_EOM(m1,m2,L1,L2,a,b,grav);
 
 
 %%
 extra.b1=50;
 extra.k1=3000;
 extra.b2=10;
-extra.k2=100000;
+extra.k2=300;
 
 init=[0;0;0;0];
 dt=0.005;
+#'$k_1$ = '+str(self.k1)+' $b_1$ = '+str(self.b1)+' $k_2$ = '+str(self.k2)+' $b_2$ = '+str(self.b2)
 
 simulatePendulumn(dX,init,dt,L1,L2,extra);
--- Python/main.py
+++ Python/main.py
@@ -13,12 +13,7 @@ import sys
 from PyQt5 import QtWidgets
 
 
-
-
-
-
 Ui_MainWindow, QMainWindow = loadUiType('dpGUI.ui')
-
 
 
 class DPSim(QMainWindow, Ui_MainWindow):
@@ -26,121 +21,145 @@ class DPSim(QMainWindow, Ui_MainWindow):
         super(DPSim, self).__init__()
         # Setup the figure with the axes plots to be used for visualization
         self.setupUi(self)
-        self.fig=Figure()
-        self.k2=220
-        self.k1=1000
-        self.b1=50
-        self.b2=20
-        self.max_y=0.1
-        self.min_y=-0.1
-        ax = self.fig.add_subplot(211, aspect='equal', autoscale_on=False,
-                     xlim=(-2.1, 2.1), ylim=(-0.2, 2.2))
+        self.fig = Figure()
+        self.k2 = 220
+        self.k1 = 1000
+        self.b1 = 50
+        self.b2 = 20
+        self.max_y = 0.1
+        self.min_y = -0.1
+        self.ax = self.fig.add_subplot(211, aspect='equal', autoscale_on=False,
+                                       xlim=(-2.1, 2.1), ylim=(-0.2, 2.2))
         self.ax2 = self.fig.add_subplot(212)
+        self.ax.set_axis_off()
         self.ax2.set_xlabel('Time (s)')
         self.ax2.set_ylabel('$\phi$ (deg)')
         self.ax2.grid(linestyle='-', linewidth='0.5', color='black')
-        line1, = ax.plot([], [], 'o-', lw=2)
+        line1, = self.ax.plot([], [], 'o-', lw=2)
         line2, = self.ax2.plot([], [])
-        line3 = self.ax2.quiver([],[],[])
+        line3, = self.ax.plot([], [])
         self.lines = [line1, line2, line3]
-        self.time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
-        #Create the pendulum object which simulates the dynamics and gives states to be plotted.
-        self.p1 = Pendulum(20, 80, 1, 0.5, 0.5, 0.5)
-        self.p1.initSim(np.array([[0],[0],[0],[0]]))
-        self.p1.setSDproperties(self.k1,self.k2,self.b1, self.b2)
+        self.time_text = self.ax.text(
+            0.02, 0.90, '', transform=self.ax.transAxes)
+        self.coef_text = self.ax.text(
+            0.78, 0.55, '', transform=self.ax.transAxes)
+        # Create the pendulum object which simulates the dynamics and gives states to be plotted.
+        self.p1 = Pendulum(20, 80, 1, 0.5, 0.5, 0.2)
+        self.p1.initSim(np.array([[0], [0], [0], [0]]))
+        self.p1.setSDproperties(self.k1, self.k2, self.b1, self.b2)
         # Create the arrays that will hold the roll and time vectors
         self.xdata, self.ydata = [0], [0]
-        self.step=15000
+        self.step = 15000
         self.dt = 0.005
-        self.duration=0.3/self.dt
-        self.magnitude=300
-        self.canvas = FigureCanvas(self.fig) # create a canvas for the figure
-        
+        self.duration = 0.3/self.dt
+        self.magnitude = 300
+        self.canvas = FigureCanvas(self.fig)  # create a canvas for the figure
+
         t0 = time()
         self.animate(0)
         t1 = time()
         interval = 1000 * self.dt - (t1 - t0)
 
         self.ani = animation.FuncAnimation(self.fig, self.animate, frames=300,
-                              interval=interval, blit=True, init_func=self.init)
-        
+                                           interval=interval, blit=True, init_func=self.init)
+
         self.exitButton = QtWidgets.QPushButton('Quit')
         self.changeButton = QtWidgets.QPushButton('Change')
         self.impulseButton = QtWidgets.QPushButton('Impulse')
-        self.resetButton  = QtWidgets.QPushButton('Reset')
-        self.stiffnessEdit = QtWidgets.QLineEdit(str(self.k2))
+        self.resetButton = QtWidgets.QPushButton('Reset')
+        self.stiffnessEdit = QtWidgets.QLineEdit('')
         self.btnLayout.addWidget(self.stiffnessEdit)
         self.btnLayout.addWidget(self.changeButton)
-        self.btnLayout.addWidget(self.exitButton)
         self.btnLayout.addWidget(self.impulseButton)
         self.btnLayout.addWidget(self.resetButton)
+        self.btnLayout.addWidget(self.exitButton)
         self.exitButton.clicked.connect(app.exit)
         self.changeButton.clicked.connect(self.changeStiffnessCallback)
         self.impulseButton.clicked.connect(self.impulseCallback)
         self.resetButton.clicked.connect(self.resetButtonCallback)
         self.exitButton.show()
         self.mplvl.addWidget(self.canvas)
-        self.canvas.draw() 
-        
+        self.canvas.draw()
+
     def init(self):
         """initialize animation"""
         self.lines[0].set_data([], [])
         self.lines[1].set_data([], [])
+        self.lines[2].set_data([], [])
         self.time_text.set_text('')
-        return self.lines[0], self.lines[1], self.time_text        
-    
-    def animate(self,i):
+        self.coef_text.set_text('')
+        return self.lines[0], self.lines[1], self.lines[2], self.time_text, self.coef_text
+
+    def animate(self, i):
         """perform animation step"""
-        #input disturbance logic
-        if(self.step<self.duration):
-            self.step+=1;
-            if( self.step<np.floor( self.duration/2)):
-                w=(self.magnitude*2/(self.duration*self.dt))*(self.dt*self.step)
+        # input disturbance logic
+        if(self.step < self.duration):
+            self.step += 1
+            if(self.step < np.floor(self.duration/2)):
+                w = (self.magnitude*2/(self.duration*self.dt)) * \
+                    (self.dt*self.step)
             else:
-                w=-(self.magnitude*2/(self.duration*self.dt))*(self.dt*self.step)+2*self.magnitude
+                w = -(self.magnitude*2/(self.duration*self.dt)) * \
+                    (self.dt*self.step)+2*self.magnitude
         else:
-            w=0
-            self.step=15000
-            
+            w = 0
+            self.step = 15000
+
         self.p1.updateState(w, self.dt)
         self.ydata.append(self.p1.state[0]*180/pi)
         self.xdata.append(self.p1.t)
         if (len(self.xdata) > 20/self.dt):
             self.xdata.pop(0)
             self.ydata.pop(0)
-        self.lines[0].set_data(*self.p1.getPosition())
+        X, Y = self.p1.getPosition()
+        self.lines[0].set_data(X, Y)
         self.lines[1].set_data(self.xdata, self.ydata)
+
+        self.lines[2].set_xdata([X[1], X[1]+w/self.magnitude, X[1]+w /
+                                 self.magnitude*0.9, X[1]+w/self.magnitude*0.9, X[1]+w/self.magnitude])
+        self.lines[2].set_ydata(
+            [Y[1], Y[1], Y[1]+w/self.magnitude*0.1, Y[1]-w/self.magnitude*0.1, Y[1]])
+
         self.ax2.set_xlim([self.xdata[0], self.p1.t])
-        if(self.ydata[-1]<self.min_y):
-            self.min_y=self.ydata[-1]
-        if(self.ydata[-1]>self.max_y):
-            self.max_y=self.ydata[-1]
+        if(self.ydata[-1] < self.min_y):
+            self.min_y = self.ydata[-1]
+        if(self.ydata[-1] > self.max_y):
+            self.max_y = self.ydata[-1]
         self.ax2.set_ylim([self.min_y*1.2, self.max_y*1.2])
         self.time_text.set_text('t = %.1f s' % self.p1.t)
-        return self.lines[0], self.lines[1], self.time_text    
+        self.coef_text.set_text('$k_1$ = '+str(self.k1)+'\n $b_1$ = '+str(
+            self.b1)+'\n $k_2$ = '+str(self.k2)+'\n $b_2$ = '+str(self.b2))
+        return self.lines[0], self.lines[1], self.lines[2], self.time_text, self.coef_text
+
     def impulseCallback(self):
-        self.step=0
+        self.step = 0
+
     def changeStiffnessCallback(self):
-        if (self.stiffnessEdit.text()[0:3]=='k1='):
-            self.k1=float(self.stiffnessEdit.text()[3:])
-        elif (self.stiffnessEdit.text()[0:3]=='k2='):
-            self.k2=float(self.stiffnessEdit.text()[3:])
-        elif (self.stiffnessEdit.text()[0:3]=='b1='):
-            self.b1=float(self.stiffnessEdit.text()[3:])
-        elif (self.stiffnessEdit.text()[0:3]=='b2='):
-            self.b2=float(self.stiffnessEdit.text()[3:])
+        if (self.stiffnessEdit.text()[0:3] == 'k1='):
+            self.k1 = float(self.stiffnessEdit.text()[3:])
+        elif (self.stiffnessEdit.text()[0:3] == 'k2='):
+            self.k2 = float(self.stiffnessEdit.text()[3:])
+        elif (self.stiffnessEdit.text()[0:3] == 'b1='):
+            self.b1 = float(self.stiffnessEdit.text()[3:])
+        elif (self.stiffnessEdit.text()[0:3] == 'b2='):
+            self.b2 = float(self.stiffnessEdit.text()[3:])
         else:
             self.stiffnessEdit.setText('Malaka!')
-        self.p1.setSDproperties(self.k1,self.k2,self.b1, self.b2)
+        self.p1.setSDproperties(self.k1, self.k2, self.b1, self.b2)
+
     def resetButtonCallback(self):
-        self.p1.initSim(np.array([[0],[0],[0],[0]]))
+        self.p1.initSim(np.array([[0], [0], [0], [0]]))
+        self.k2 = 220
+        self.k1 = 1000
+        self.b1 = 50
+        self.b2 = 20
+        self.p1.setSDproperties(self.k1, self.k2, self.b1, self.b2)
         self.xdata.clear()
         self.ydata.clear()
+
+
 app = QtWidgets.QApplication(sys.argv)
 sim = DPSim()
 
 sim.show()
-app.exec_()    
-  
-    
-
+app.exec_()
